0c656b3433a4f29e04b09a82d1de4429bc4cae00
Redandan <coconut0989@gmail.com>
2026-06-05 11:37:33 +0800

    Disable liquidation WS in test context

--- src/test/java/com/agora/trading/TradingApiApplicationTests.java
+++ src/test/java/com/agora/trading/TradingApiApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(properties = {
         "spring.ai.model.chat=none",
         "spring.ai.chat.client.enabled=false",
+        "market.liquidation-ws.enabled=false",
         "spring.datasource.url=jdbc:h2:mem:trading-test;MODE=MySQL;DATABASE_TO_LOWER=TRUE;NON_KEYWORDS=VALUE;DB_CLOSE_DELAY=-1",
         "spring.datasource.driver-class-name=org.h2.Driver",
         "spring.jpa.hibernate.ddl-auto=create-drop"

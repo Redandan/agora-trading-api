5394965f7ec02d1e5301821c54455b919e2f46a4
Redandan <coconut0989@gmail.com>
2026-06-05 12:44:40 +0800

    Disable short squeeze schedulers in local checks

--- src/test/java/com/agora/trading/TradingApiApplicationTests.java
+++ src/test/java/com/agora/trading/TradingApiApplicationTests.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
         "meta-control.attribution.enabled=false",
         "meta-control.composite-indicator.scheduler-enabled=false",
         "meta-control.ml-materialized-refresh.startup-check-enabled=false",
+        "trading.short-squeeze-alert.enabled=false",
+        "trading.short-squeeze-alert.taker-buy-collector-enabled=false",
         "spring.datasource.url=jdbc:h2:mem:trading-test;MODE=MySQL;DATABASE_TO_LOWER=TRUE;NON_KEYWORDS=VALUE;DB_CLOSE_DELAY=-1",
         "spring.datasource.driver-class-name=org.h2.Driver",
         "spring.jpa.hibernate.ddl-auto=create-drop"
